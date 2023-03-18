d90ac64776f48a8fe640404155fabab7e17f9751
Courtney H <cmh0101@auburn.edu>
2023-03-18 16:59:45 -0500

    I created the script for the origin.txt file and included helpful information on how to create the script.

---
+++ origin.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+
+
+#file: origin.txt
+
+#find all occurances of heritable, inherit, inheritance, and other forms 
+#of these words using a regular expression. including caps
+
+#format: {line number it was found on} SPACE(\t) {word} NEWLINE(\n) 
+
+#put meaningful units of code into functions
+#make script importable using "if __name__ == '__main__':" flow control
+#write informative docstrings to document script and functions
+#Try to keep code flexible/dynamic so it can be used for other files
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#flow control?
+if __name__ == '__main__':
