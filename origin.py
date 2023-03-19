1aba03c67adfd2c31a7455f7a8eaf50a1b99cca2
Courtney H <cmh0101@auburn.edu>
2023-03-18 20:29:37 -0500

    Edited doc string and added if __name__ == '__main__': and my function name at the end of the script. No errors are occuring but I am not sure if I did the importable line correctly.

--- origin.py
+++ origin.py
@@ -17,40 +17,27 @@ import re
 
 
 
-#def get_wordform(file)
-#	"""Finds the words 'heritable' 'inherit', 'inheritance' and 
-#	other forms of the words and puts them into a new file including 
-#	the line that the word was found on and a tab inbetween. This is 
-#	done using regular expression. Each occurance needs to be on a new 
-#	line. 
-#	"""
-
-#filename = "origin.txt"
-output_file = "origin_list.txt"
-
-with open('origin.txt', 'r') as f_in, open(output_file,"w") as f_out:
-    for line_num, line in enumerate(f_in, start=1):
-        words = line.split()
-        for word in words:
-            if "herit" in word.lower():
-                f_out.write(f"{line_num}\t{word}\n")
+def get_wordform():
+        """Finds the words 'heritable' 'inherit', 'inheritance' and 
+        other forms of the words and puts them into a new file including 
+        the line that the word was found on and a tab inbetween. This is 
+        done using regular expression. Each occurance needs to be on a new 
+        line. 
+        """
 
 
+        output_file = "origin_list.txt"
+
+        with open('origin.txt', 'r') as f_in, open(output_file,"w") as f_out:
+            for line_num, line in enumerate(f_in, start=1):
+                words = line.split()
+                for word in words:
+                    if "herit" in word.lower():
+                        f_out.write(f"{line_num}\t{word}\n")
 
 
 
+if __name__ == '__main__':
+    print(get_wordform())
 
 
-
-
-
-
-
-
-
-
-
-
-
-#flow control?
-#if __name__ == '__main__':
