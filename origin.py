f8105db63ce5e36deb1e925db4b665e638932b5f
Courtney H <cmh0101@auburn.edu>
2023-03-18 19:54:04 -0500

    I created a working script to identify all the occurances of words containing "herit" and now I will go in to edit my doc string and make the file importable.

--- origin.py
+++ origin.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import re
 
 #file: origin.txt
 
@@ -16,8 +17,23 @@
 
 
 
+#def get_wordform(file)
+#	"""Finds the words 'heritable' 'inherit', 'inheritance' and 
+#	other forms of the words and puts them into a new file including 
+#	the line that the word was found on and a tab inbetween. This is 
+#	done using regular expression. Each occurance needs to be on a new 
+#	line. 
+#	"""
 
+#filename = "origin.txt"
+output_file = "origin_list.txt"
 
+with open('origin.txt', 'r') as f_in, open(output_file,"w") as f_out:
+    for line_num, line in enumerate(f_in, start=1):
+        words = line.split()
+        for word in words:
+            if "herit" in word.lower():
+                f_out.write(f"{line_num}\t{word}\n")
 
 
 
@@ -37,4 +53,4 @@
 
 
 #flow control?
-if __name__ == '__main__':
+#if __name__ == '__main__':
